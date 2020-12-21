1d60856e662387bd12579554a227a055d0acb2e6
shwet <shwet@host.docker.internal>
2020-12-21 10:04:35 -0500

    added comment

--- src/main/java/com/hackerrank/orm/Application.java
+++ src/main/java/com/hackerrank/orm/Application.java
@@ -1,8 +1,9 @@
 package com.hackerrank.orm;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//main
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
